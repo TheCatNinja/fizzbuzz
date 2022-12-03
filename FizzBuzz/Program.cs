9e83b0764d51b3332d3ae12cbf81c2a9d0aefe9d
TheCatNinja <milewska2006@gmail.com>
2022-12-03 11:50:42 +0100

    first commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿int len = 100;
+string[] array = new string[len];
+
+for (int i = 0; i < len; i++)
+{
+    int y = i + 1;
+    if (y % 3 == 0 && y % 5 == 0)
+        array[i] = "FizzBuzz";
+    else if (y % 3 == 0)
+        array[i] = "Fizz";
+    else if (y % 5 == 0)
+        array[i] = "Buzz";
+    else
+        array[i] = y.ToString();
+    Console.WriteLine($"{y} - {array[i]}");
+}
+
+Console.Read();
